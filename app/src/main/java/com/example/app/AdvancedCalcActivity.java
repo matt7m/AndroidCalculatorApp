5e194433c9257593c2be9a1482d81a59f20f450c
matim1324 <matim1324@gmail.com>
2021-11-21 00:01:44 +0100

    Navigation to advanced calculator view.

--- app/src/main/java/com/example/app/AdvancedCalcActivity.java
+++ app/src/main/java/com/example/app/AdvancedCalcActivity.java
@@ -2,16 +2,43 @@ package com.example.app;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdvancedCalcActivity extends AppCompatActivity {
+    private TextView Screen;
+    private Button btnBack, btnAdd, btnSub, btnMlp, btnDiv, btnClear, btnDot, btnChange;
+    private Button btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine, btnZero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.advanced_calculator);
 
+        init_buttons();
+        btnBack.setOnClickListener(view -> finish());
     }
 
+    private void init_buttons(){
+        btnZero = findViewById(R.id.bt0);
+        btnOne = findViewById(R.id.bt1);
+        btnTwo = findViewById(R.id.bt2);
+        btnThree = findViewById(R.id.bt3);
+        btnFour = findViewById(R.id.bt4);
+        btnFive = findViewById(R.id.bt5);
+        btnSix = findViewById(R.id.bt6);
+        btnSeven = findViewById(R.id.bt7);
+        btnEight = findViewById(R.id.bt8);
+        btnNine = findViewById(R.id.bt9);
+
+        btnBack = findViewById(R.id.bt_back);
+        btnAdd = findViewById(R.id.bt_add);
+        btnSub = findViewById(R.id.bt_sub);
+        btnMlp = findViewById(R.id.bt_mlp);
+        btnDiv = findViewById(R.id.bt_div);
+        btnDot = findViewById(R.id.bt_dot);
+        btnClear = findViewById(R.id.bt_clr);
+        btnChange = findViewById(R.id.bt_pm);
+    }
 
 }
--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -40,6 +40,5 @@ public class MainActivity extends AppCompatActivity {
             finish();
             System.exit(0);
         });
-
     }
 }
